3c9228f9e9d116b3d5224bc065468cfb5f38d968
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2025-07-28 13:41:37 +0530

    fix: add json support for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,26 @@
+// @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import json from '@eslint/json'
 
-export default withNuxt()
+export default withNuxt(
+  {
+    plugins: {
+      json,
+    },
+  },
+  {
+    files: ['**/*.json'],
+    language: 'json/json',
+    rules: {
+      'json/no-duplicate-keys': 'error',
+      'json/no-empty-keys': 'error',
+    },
+  },
+  {
+    files: ['**/*.jsonc'],
+    language: 'json/jsonc', // JSON with comments
+    rules: {
+      'json/no-duplicate-keys': 'error',
+    },
+  },
+)
