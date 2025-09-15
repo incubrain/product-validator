2dea4644a6939eef9221836e20d2765dc9a83ae8
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2025-09-15 15:52:38 +0530

    chore: remove unused packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,4 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
-import json from '@eslint/json'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt(
-  {
-    plugins: {
-      json,
-    },
-  },
-  {
-    files: ['**/*.json'],
-    language: 'json/json',
-    rules: {
-      'json/no-duplicate-keys': 'error',
-      'json/no-empty-keys': 'error',
-    },
-  },
-  {
-    files: ['**/*.jsonc'],
-    language: 'json/jsonc', // JSON with comments
-    rules: {
-      'json/no-duplicate-keys': 'error',
-    },
-  },
-)
+export default withNuxt();
