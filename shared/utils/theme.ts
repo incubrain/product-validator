cf009b443ed927393182392d417221996d1d1f1e
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2025-09-15 13:32:07 +0530

    fix: ExtractVariants handles booleans

--- shared/utils/theme.ts
+++ shared/utils/theme.ts
@@ -1,12 +1,18 @@
 // shared/utils/theme.ts
 export type ExtractVariants<T> = T extends Record<string, any>
   ? {
-      [K in keyof T]: keyof T[K]
+      [K in keyof T]: T[K] extends Record<string | number, any>
+        ? keyof T[K] extends string
+          ? keyof T[K] extends 'true' | 'false'
+            ? boolean // Convert "true"/"false" keys to boolean type
+            : keyof T[K]
+          : keyof T[K]
+        : keyof T[K];
     }
-  : never
+  : never;
 
 export type ExtractSlots<T> = T extends Record<string, any>
   ? {
-      [K in keyof T]: string | string[]
+      [K in keyof T]: string | string[];
     }
-  : never
+  : never;
