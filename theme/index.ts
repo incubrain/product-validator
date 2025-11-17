9df82bfda08808126f6c316eda39f10d9407b449
Mac <82544364+Drew-Macgibbon@users.noreply.github.com>
2025-11-17 13:12:31 +0530

    fix: duplicate videoStyles export

--- theme/index.ts
+++ theme/index.ts
@@ -6,5 +6,4 @@ export type {
 export { pageColumnsWrapperStyles } from './ui/page/columns';
 
 // CUSTOM COMPONENTS
-export type { VideoVariants, VideoSlots } from './video';
-export { videoStyles } from './video';
+export * from './video';
